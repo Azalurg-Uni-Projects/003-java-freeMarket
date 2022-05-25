3ab36cb96eafff8eac5020e4bad0b915e8260e96
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-05-25 08:21:21 +0200

    Update Main.java

--- Market/main/Main.java
+++ Market/main/Main.java
@@ -1,23 +1,38 @@
 package Market.main;
 
+import Market.participants.buyer.Buyer;
 import Market.participants.seller.Seller;
 import Market.products.Product;
 import Market.products.ProductsFactory;
 import Market.products.examples.Bitcoin;
+import Market.publisher.Publisher;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
         ProductsFactory productsFactory = new ProductsFactory();
-        Seller s = new Seller(0.1);
-        Product p = productsFactory.getProduct("Silver");
-        s.addProduct(p);
-        s.addProduct(productsFactory.getProduct("Gold"));
+        Seller s = new Seller(0.1);;
         s.addProduct(productsFactory.getProduct("Silver"));
-        s.sell("Gold");
-        s.sell("Silver");
-        s.sell("Silver");
+        s.printProductsList();
+        // ===========================
+        Publisher publisher = new Publisher();
+        Buyer b1 = new Buyer(1);
+        b1.addProduct(productsFactory.getProduct("Silver"));
+        publisher.subscribe("Silver", b1);
+
+        Buyer b2 = new Buyer(1);
+        b2.addProduct(productsFactory.getProduct("Silver"));
+        publisher.subscribe("Silver", b2);
+
+        System.out.println(publisher);
+        publisher.notify("Silver", s, s.getProductHashMap().get("Silver"));
+        s.printProductsList();
+        publisher.notify("Silver", s, s.getProductHashMap().get("Silver"));
+        s.printProductsList();
+        publisher.notify("Silver", s, s.getProductHashMap().get("Silver"));
+        s.printProductsList();
+        publisher.notify("Silver", s, s.getProductHashMap().get("Silver"));
         s.printProductsList();
 
 
